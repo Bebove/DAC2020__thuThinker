4f907088cb13cdec91584a2ae77517e1c05b3bf3
adfad1 <31584633+adfad1@users.noreply.github.com>
2020-03-29 21:18:56 +0800

    add conv3x3

--- code/dac.h
+++ code/dac.h
@@ -4,32 +4,26 @@
 
 typedef ap_int<8> int8;
 typedef ap_uint<9> uint9;
+typedef ap_uint<20> uint20;
+typedef ap_uint<16> uint16;
 typedef ap_fixed<32, 1, AP_RND, AP_SAT> fix_32_1;
-typedef int bs_type;//bias
-typedef fix_32_1 scale_type;//scale type
-typedef int8 wt_type;//weight
-typedef int8 fm_type;//feature map data type
-typedef int fms_type;//feature map for scale type aaa
+typedef ap_fixed<11, 4, AP_RND, AP_SAT> wt_type;//weight type
+typedef ap_fixed<9,  3, AP_RND, AP_SAT> fm_type;//feature map data type
+typedef ap_fixed<11, 4, AP_RND, AP_SAT> bs_type;//bias type
 /*
-fm_type fm_buf[96][82][50];
-fms_type fms_buf[96][82][50];
+fm_type fm_buf1[96*82*50];
+fm_type fm_buf2[96*82*50];
 wt_type wt_buf[480*80];
-scale_type scale[480];
 bs_type bias[480];
 */
-void conv_pre(fms_type out_buf[96][82][50],
+void load_bias(fm_type out_buf[96][82][50],
 		bs_type bias[480],
-		uint9 w, uint9 h ,uint9 ch
+		uint9 w_o, uint9 h_o,uint9 ch_o
 		);
-/*
-void conv(fm_type in_buf[96][82][50],
-		fms_type out_buf[96][82][50],
-		uint9 w, uint9 h ,uint9 ch
-		);
-*/
-void Conv(ap_fixed<32,16, AP_RND, AP_SAT> re[2],
-		ap_uint<16> c[2],
 
-		ap_uint<8> a[2],
-		ap_uint<8> b[2],
-		ap_fixed<32,16, AP_RND, AP_SAT> h[2]);
+void conv3x3(fm_type in_buf[96*82*50],
+		fm_type out_buf[96*82*50],
+		wt_type weight[480*80],
+		uint9 w_i, uint9 h_i ,uint9 ch_i, //input feature map scale
+		uint9 w_o, uint9 h_o ,uint9 ch_o  //output feature map scale
+		);
