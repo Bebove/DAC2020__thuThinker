49d1f1576843922632ad156589ad097ff9754ebd
Bebove <shadowsocksxposed@gmail.com>
2020-04-02 22:11:58 +0800

    add test_data generate code// add simulation code

--- code/tb.cc
+++ code/tb.cc
@@ -1,1 +1,35 @@
+#include "dac.h"
+//#include <malloc.h>
+uint16 IMG[3*(320+4)*(192+4)*4];
 
+
+
+void fold_data(uint16 IMG[3*(320+4)*(192+4)*4])
+{
+	int ip1;
+	ip1=3*(320+4)*(192+4)*4;
+	float data;
+	data=4/float(ip1);
+	float temp;
+	temp=-2;
+
+	//fm_type *temp_img ;
+	//temp_img = (fm_type *)malloc(3*(320+4)*(192+4)*4* sizeof(fm_type));
+	int i;
+	i=0;
+	while(i<ip1)
+	{
+		IMG[i].range(8, 0)=((fm_type)temp).range(8, 0);
+		temp=temp+data;
+		i=i+1;
+	}
+
+}
+
+int main()
+{
+
+	fold_data(IMG);
+    SkyNet(	 IMG );
+    return 0;
+}
