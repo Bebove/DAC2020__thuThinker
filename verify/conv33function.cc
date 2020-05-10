38acd70b34347a743abcb4f9ff9b8b1cb44cd5d7
adfad1 <31584633+adfad1@users.noreply.github.com>
2020-05-10 15:18:01 +0800

    l1,l2 testbench

---
+++ verify/conv33function.cc
@@ -0,0 +1,89 @@
+#include "dac.h"
+#include <stdio.h>
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <math.h>
+#include <fstream>
+#include <hls_math.h>
+#include <string.h>
+using namespace std;
+
+inline fm_type relu_single( fm_type d ,int relu) {
+	if(relu ==1){
+	if( d > 6 )
+		return 6;
+	if( d < 0 )
+		return 0;}
+	return d;
+}
+//fm_type fm_buf_for33[4][8][49][81];
+void conv3x3(fm_type in_buf[80][49][81],
+		fm_type out_buf[8][49][81],
+		wt_type weight[8][32][3][3]
+		){
+#pragma HLS array_partition variable=in_buf dim=1 complete
+#pragma HLS array_partition variable=out_buf dim=1 complete
+#pragma HLS array_partition variable=weight dim=1 complete
+#pragma HLS array_partition variable=weight dim=2 complete
+
+
+    for(int y=0;y<3;y++){
+        for (int x=0;x<3;x++){
+            for(int h=0,hi=0; h<24; h++,hi+=2){
+                for(int w=0,wi=0; w<40; w++,wi+=2){
+#pragma HLS PIPELINE
+                    for(int cho=0; cho<8; cho++){
+                        for(int chi=0;chi<32;chi++){
+                            out_buf[cho][h][w]+=weight[cho][chi][y][x]*in_buf[chi][hi+y][wi+x];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void set_bias_conv3x3( fm_type buf[4][8][49][81], bs_type bias[80])
+{
+#pragma HLS array_partition variable=buf dim=1 complete
+//#pragma HLS array_partition variable=bias dim=1 complete
+	for(int h = 1; h <= 49; h+=1) {
+		for(int w = 1; w <= 81; w++) {
+#pragma HLS pipeline
+			for(int c = 0; c < 4; c++) {
+				for(int cd = 0; cd < 8; cd++) {
+#pragma HLS unroll
+				buf[c][cd][h  ][w] = bias[c*8+cd];
+				}
+			}
+		}
+	}
+}
+
+void load_weight_conv3x3( wt_type dest[4][8][32][3][3], uint512 src[500][3][3],int ofset)
+{
+	//this function should be able to load 32*32*3*3 weight, so the src port should have the size of 3x3x32
+	for(int m = 0; m < 3; m++)
+	{
+		for(int n = 0; n < 3; n++)
+		{
+			for(int co = 0; co < 4; co++)
+			{
+				for(int coindex = 0; coindex < 8; coindex++)
+				{
+#pragma HLS pipeline
+				uint512 DATA = 0;
+
+				DATA.range(511, 0) = src[co*8+coindex+	ofset	][m][n].range(511, 0);        //the ofset function in there
+				for(int ci = 0; ci < 32; ci++)
+				{
+#pragma HLS unroll
+					dest[co][coindex][ci][m][n].range(10, 0) = DATA.range(10 + ci*16, ci*16);  //this means for 3x3 conv, every index(0-500) contains 32 3x3 weight
+				}
+				}
+
+			}
+		}
+	}
+}
---
+++ verify/dac.cc
@@ -0,0 +1,169 @@
+#include "dac.h"
+#include <stdio.h>
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <math.h>
+#include <fstream>
+#include <hls_math.h>
+#include <string.h>
+using namespace std;
+fm_type fm_buf1[80][50][82];
+fm_type fm_buf2[80][50][82];
+fm_type fm_buf3[80][50][82];
+fm_type fm_buf4[80][50][82];
+
+
+
+
+wt_type wt_buf3[32][32][3][3];
+wt_type dwt_buf3[96][3][3];
+wt_type wt_buf1[16][16];
+wt_type wt_buf1a[16][16];
+bs_type bias[80];
+bs_type bias2[80];
+bs_type bias3[80];
+
+/*
+void padding_offset(int x, int y, int &offsetx, int &offsety)
+{
+	if(x==4){offsetx=1;}
+	else{offsetx=0;}
+
+	if(y==4){offsety=1;}
+	else{offsety=0;}
+}
+*/
+
+ 
+ 
+
+void Thinker(	uint16 image_in_raw_pad[imagesize],
+				uint512		w_port_3x3[500][3][3],
+				uint256     w_port_1x1[500][16],
+				uint256     bias_port[500][5],
+
+				uint16 ddr1 [imagesize],
+				uint16 debug[2])
+{
+ 	
+#pragma HLS INTERFACE m_axi depth=3*(320+2)*(192+2) 	port=image_in_raw_pad			offset=slave	bundle=INPUT
+#pragma HLS INTERFACE m_axi depth=500*3*3 	port=w_port_3x3			offset=slave	bundle=INPUT
+	//16000 3x3 kernel
+	//every uint512 store 32 11bit number, so we need 500*3*3. the uint32 store input channel data
+#pragma HLS INTERFACE m_axi depth=500 	port=w_port_1x1			offset=slave	bundle=INPUT
+	//every uint256 store 16 number, so 1 index is a full 16x16x1x1 data
+#pragma HLS INTERFACE m_axi depth=500 	port=bias_port			offset=slave	bundle=INPUT
+	//So we use a ddr of size at least 48*(20*8)*(12*8)
+#pragma HLS INTERFACE m_axi depth=3*(320+2)*(192+2) 	port=ddr1			offset=slave	bundle=INPUT
+
+
+#pragma HLS INTERFACE s_axilite register	port=return
+#pragma HLS INTERFACE m_axi depth=2			port=debug				offset=slave	bundle=OUTPUT
+
+
+
+#pragma HLS ALLOCATION instances=CONV_1x1			 		limit=1 function
+#pragma HLS ALLOCATION instances=dw_conv_2			 		limit=1 function
+#pragma HLS ALLOCATION instances=dw_conv_1			 		limit=1 function
+
+
+	//layer 307 310
+	load_weight_conv1x1(wt_buf1, w_port_1x1[0]);   //load  weight for conv1x1 307  		,   	which is store at the index 0
+	load_dwweight_conv3x3(dwt_buf3, w_port_3x3,0); //load  weight for dwconv3x3 310		,	    which is store at the index 0,1,2
+
+	load_bias_from_axi(bias, bias_port[0]);        //load  bias   for conv1x1 ,	    which is store at the index 0
+	load_bias_from_axi(bias2, bias_port[1]);       //load  bias   for conv3x3 ,	    which is store at the index 1
+
+
+
+	int offsetx;
+	int offsety;
+
+	int relu11=0;
+	int relu13=0;
+	int relu33=0;
+	int channelnumber=6;
+	int channeloffset=0;
+
+	for(int x=0;x<8;x++)
+	{
+
+		for(int y=0;y<8;y++)
+		{
+			//x: h index, x*48*320=offset
+			//y: w index, y*80=offset
+			if(x==4){offsetx=1;}
+			else{offsetx=0;}
+
+			if(y==4){offsety=1;}
+			else{offsety=0;}
+
+			//load_img(fm_buf1, image_in_raw_pad, x,  y,  offsetx,  offsety); //load the first small part of image
+			aload_img(fm_buf1, image_in_raw_pad, x,  y,  offsetx,  offsety,3,0,
+								(320+2)*2,
+								(192+2)*2,
+								82,
+								50);
+			set_bias_conv1x1( fm_buf2, bias,x,y);
+			CONV_1x1(fm_buf1,fm_buf2,wt_buf1,0,0,0); //level 1
+
+			set_dwbias_conv3x3(fm_buf3,bias2);
+			dw_conv_2(fm_buf2,fm_buf3,dwt_buf3,6,1); //level 2
+
+
+			deload_img(fm_buf3, ddr1, x,  y,  offsetx,  offsety,channelnumber,channeloffset,relu33,
+								(160+2)*2,
+								(96+2)*2,
+								40,
+								24);
+
+		}
+	}
+
+/*
+
+	//layer 313
+	load_bias_from_axi(bias, bias_port[2]);   //load  bias  for the first conv1x1 ,	 which is store at the index 2
+	load_bias_from_axi(bias2,bias_port[3]); //bias for dw conv3x3
+	load_bias_from_axi(bias3,bias_port[4]); //bias for the second conv1x1
+	load_weight_conv1x1(wt_buf1,w_port_1x1[1]);
+	load_weight_conv1x1(wt_buf1a,w_port_1x1[2]);
+	load_dwweight_conv3x3(dwt_buf3,w_port_3x3,3);
+	
+	channelnumber=8;//only for the feature map to be stored in ddr
+	channeloffset=0;
+	
+	for(int x=0;x<4;x++){
+		for(int y=0;y<4;y++){
+
+			if(x==2){offsetx=1;}
+			else{offsetx=0;}
+
+			if(y==2){offsety=1;}
+			else{offsety=0;}
+
+			aload_img(fm_buf1,ddr1,x,y,offsetx,offsety,6,0,
+						(160+2)*2,
+						(96+2)*2,
+						82,
+						50);
+			set_bias_conv1x1(fm_buf2,bias,x,y);
+			CONV_1x1(fm_buf1,fm_buf2,wt_buf1,0,0,0); //level 3
+
+			set_dwbias_conv3x3(fm_buf3,bias2);
+			dw_conv_1(fm_buf2,fm_buf3,dwt_buf3,16,0); //level 4
+
+			set_bias_conv1x1(fm_buf4,bias3,x,y);
+			CONV_1x1(fm_buf3,fm_buf4,wt_buf1a,0,0,1); // level 5
+
+			deload_img(fm_buf4, ddr1, x, y,  offsetx, offsety,channelnumber,channeloffset,0,
+				(160+2)*2,
+				(96+2)*2,
+				80,
+				48);
+		}
+	}
+
+	debug[0]=0.1;
+*/
+}
